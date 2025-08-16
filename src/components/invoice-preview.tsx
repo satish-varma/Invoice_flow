55760718d5b6183968f84484af767c56d589c12f
thegut guru <thegutguru.in@gmail.com>
2025-08-16 17:49:06 +0000

    nice you covered most of the fields, remove the unwanted line from the i

--- src/components/invoice-preview.tsx
+++ src/components/invoice-preview.tsx
@@ -23,7 +23,7 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
         str += (Number(n[3]) != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
         str += (Number(n[4]) != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
         str += (Number(n[5]) != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
-        return str.trim().split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ') + ' Rupees';
+        return str.trim().split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ') + ' Only';
     };
     
     return (
@@ -33,64 +33,49 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                     <div className='text-center p-2 border-b-2 border-black'>
                         <CardTitle className="text-xl font-bold tracking-tight">BILL TO SUPPLY</CardTitle>
                     </div>
-                    <div className='flex justify-between items-center p-2 border-b-2 border-black'>
+                    <div className='flex justify-between items-center p-2'>
                         <p className='font-bold text-lg'>THE GUT GURU</p>
                         <p className='text-xs'>Original For recipient</p>
                     </div>
                 </CardHeader>
                 <CardContent className="p-0">
-                    <div className="grid grid-cols-2">
-                        <div className="border-r-2 border-black">
-                            <div className='grid grid-cols-[80px_1fr]'>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>GSTIN</div>
-                                <div className='p-1 border-b-2 border-black'>36DDTPJ6536D1Z8</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Branch</div>
-                                <div className='p-1 border-b-2 border-black'>Telangana</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>PAN</div>
-                                <div className='p-1 border-b-2 border-black'>DDTPJ6536D</div>
-                                <div className='font-bold p-1 border-r-2 border-black'>Address</div>
-                                <div className='p-1'>H NO.6-46/3/A, Venkateswarao nagar, Chanda Nagar, Hyderabad-500050</div>
-                            </div>
+                    <div className="grid grid-cols-2 border-y-2 border-black">
+                        <div className="border-r-2 border-black p-2">
+                             <p className='font-bold'>From:</p>
+                             <p className='font-bold'>THE GUT GURU</p>
+                             <p className='text-sm' style={{whiteSpace: 'pre-wrap'}}>H NO.6-46/3/A, Venkateswarao nagar, Chanda Nagar, Hyderabad-500050</p>
+                             <p className='text-sm'><span className='font-bold'>GSTIN:</span> 36DDTPJ6536D1Z8</p>
+                             <p className='text-sm'><span className='font-bold'>PAN:</span> DDTPJ6536D</p>
                         </div>
-                        <div >
-                            <div className='grid grid-cols-[100px_1fr] border-b-2 border-black'>
-                                <div className='font-bold p-1 border-r-2 border-black'>Invoice Date</div>
-                                <div className='p-1'>{invoice.date ? format(new Date(invoice.date), "P") : ''}</div>
-                                <div className='font-bold p-1 border-b-2 border-t-2 border-r-2 border-black'>Invoice No.</div>
-                                <div className='p-1 border-b-2 border-t-2 border-black'>{invoice.invoiceNumber}</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Period</div>
-                                <div className='p-1 border-b-2 border-black'>{invoice.period}</div>
-                                <div className='font-bold p-1 border-r-2 border-black'>DELIVERY</div>
-                                <div className='p-1'>{invoice.delivery}</div>
+                        <div className="p-2">
+                             <div className='grid grid-cols-[100px_1fr]'>
+                                <div className='font-bold'>Invoice Date:</div>
+                                <div>{invoice.date ? format(new Date(invoice.date), "P") : ''}</div>
+                                <div className='font-bold'>Invoice No.:</div>
+                                <div>{invoice.invoiceNumber}</div>
+                                <div className='font-bold'>Period:</div>
+                                <div>{invoice.period}</div>
+                                <div className='font-bold'>Delivery:</div>
+                                <div>{invoice.delivery}</div>
                             </div>
                         </div>
                     </div>
-                    <div className="grid grid-cols-2 border-t-2 border-black">
-                        <div className="border-r-2 border-black">
-                             <div className='grid grid-cols-[80px_1fr]'>
-                                <div className='font-bold p-1 border-b-2 border-black bg-gray-200 col-span-2'>Circle-10</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Name</div>
-                                <div className='p-1 border-b-2 border-black font-bold'>{invoice.billToName}</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Address</div>
-                                <div className='p-1 border-b-2 border-black' style={{whiteSpace: 'pre-wrap'}}>{invoice.billToAddress}</div>
-                                <div className='font-bold p-1 border-r-2 border-black'>GST NO</div>
-                                <div className='p-1'>{invoice.billToGst}</div>
-                             </div>
+                    <div className="grid grid-cols-2 border-b-2 border-black">
+                        <div className="border-r-2 border-black p-2">
+                            <p className='font-bold p-1 bg-gray-200'>Bill To:</p>
+                            <p className='font-bold'>{invoice.billToName}</p>
+                            <p style={{whiteSpace: 'pre-wrap'}}>{invoice.billToAddress}</p>
+                            <p><span className='font-bold'>GST NO:</span> {invoice.billToGst}</p>
                         </div>
-                         <div>
-                            <div className='grid grid-cols-[80px_1fr]'>
-                                <div className='font-bold p-1 border-b-2 border-black bg-gray-200 col-span-2'>SHIP TO {invoice.shipToName}</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>NAME</div>
-                                <div className='p-1 border-b-2 border-black font-bold'>{invoice.shipToName}</div>
-                                <div className='font-bold p-1 border-b-2 border-r-2 border-black'>ADDRESS</div>
-                                <div className='p-1 border-b-2 border-black' style={{whiteSpace: 'pre-wrap'}}>{invoice.shipToAddress}</div>
-                                <div className='font-bold p-1 border-r-2 border-black'>GSTIN</div>
-                                <div className='p-1'>{invoice.shipToGst}</div>
-                             </div>
+                         <div className='p-2'>
+                            <p className='font-bold p-1 bg-gray-200'>Ship To:</p>
+                            <p className='font-bold'>{invoice.shipToName}</p>
+                            <p style={{whiteSpace: 'pre-wrap'}}>{invoice.shipToAddress}</p>
+                            <p><span className='font-bold'>GSTIN:</span> {invoice.shipToGst}</p>
                         </div>
                     </div>
                     
-                    <div className="mt-0 border-t-2 border-black">
+                    <div className="mt-0">
                         <Table>
                         <TableHeader>
                             <TableRow className='border-b-2 border-black'>
@@ -103,7 +88,7 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                         </TableHeader>
                         <TableBody>
                             {invoice.lineItems.map((item, index) => (
-                            <TableRow key={index} className="border-b-2 border-black">
+                            <TableRow key={index} className="border-b border-black/20">
                                 <TableCell className='border-r-2 border-black'>{index + 1}</TableCell>
                                 <TableCell className='border-r-2 border-black'>{item.name}</TableCell>
                                 <TableCell className="text-right border-r-2 border-black">{item.quantity}</TableCell>
@@ -111,10 +96,8 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                                 <TableCell className="font-medium text-right">{(item.quantity * item.price).toFixed(2)}</TableCell>
                             </TableRow>
                             ))}
-                             <TableRow className="border-b-2 border-black">
-                                <TableCell className='border-r-2 border-black'></TableCell>
-                                <TableCell className='border-r-2 border-black'></TableCell>
-                                <TableCell className="text-right border-r-2 border-black"></TableCell>
+                             <TableRow className="border-y-2 border-black">
+                                <TableCell className='border-r-2 border-black' colSpan={3}></TableCell>
                                 <TableCell className="text-right border-r-2 border-black font-bold">Total</TableCell>
                                 <TableCell className="font-medium text-right font-bold">{invoice.subtotal.toFixed(2)}</TableCell>
                             </TableRow>
@@ -126,42 +109,31 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                            <span className='font-bold'>In Words</span>: {inWords(invoice.total)}
                         </div>
                         <div>
-                            <div className='grid grid-cols-2'>
-                                <div className='p-1 border-r-2 border-black'>Hunger Box</div>
-                                <div className='p-1 text-right'>-</div>
-                            </div>
-                            <div className='grid grid-cols-2 border-t-2 border-black'>
+                             <div className='grid grid-cols-2 border-t-2 border-black'>
                                 <div className='p-1 border-r-2 border-black font-bold'>Total Amount</div>
                                 <div className='p-1 text-right font-bold'>{invoice.total.toFixed(2)}</div>
                             </div>
                         </div>
                     </div>
-                     <div className='text-sm'>
+                     <div className='text-sm p-2'>
+                        <p className='font-semibold'>Bank Details</p>
                         <div className='grid grid-cols-[150px_1fr]'>
-                            <div className='font-bold p-1 border-b border-r-2 border-black'>Beneficiary Name:</div>
-                            <div className='p-1 border-b border-black'>THE GUT GURU</div>
-                        </div>
-                        <div className='grid grid-cols-[150px_1fr]'>
-                            <div className='font-bold p-1 border-b border-r-2 border-black'>IFSC Code:</div>
-                            <div className='p-1 border-b border-black'>HDFC0000045</div>
-                        </div>
-                        <div className='grid grid-cols-[150px_1fr]'>
-                            <div className='font-bold p-1 border-b border-r-2 border-black'>Account Number:</div>
-                            <div className='p-1 border-b border-black'>50200095177481</div>
-                        </div>
-                         <div className='grid grid-cols-[150px_1fr]'>
-                            <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Branch Branch:</div>
-                            <div className='p-1 border-b-2 border-black'>HYDERABAD - CHANDA NAGAR</div>
-                        </div>
-                         <div className='grid grid-cols-[150px_1fr]'>
-                            <div className='font-bold p-1 border-b-2 border-r-2 border-black'>Bank Name:</div>
-                            <div className='p-1 border-b-2 border-black'>HDFC BANK LTD</div>
+                            <div className='font-bold'>Beneficiary Name:</div>
+                            <div>THE GUT GURU</div>
+                            <div className='font-bold'>Bank Name:</div>
+                            <div>HDFC BANK LTD</div>
+                            <div className='font-bold'>Account Number:</div>
+                            <div>50200095177481</div>
+                            <div className='font-bold'>IFSC Code:</div>
+                            <div>HDFC0000045</div>
+                            <div className='font-bold'>Branch:</div>
+                            <div>HYDERABAD - CHANDA NAGAR</div>
                         </div>
                     </div>
                 </CardContent>
-                <CardFooter className="p-0 flex-col items-start gap-4">
+                <CardFooter className="p-0 flex-col items-start gap-4 border-t-2 border-black">
                     <div className='w-full'>
-                         <div className='grid grid-cols-[150px_1fr]'>
+                         <div className='grid grid-cols-2'>
                             <div className='p-1 border-r-2 border-black'>For THE GUT GURU</div>
                             <div className='p-1'></div>
                          </div>
@@ -170,18 +142,9 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                             <p className='text-sm'>Authorized Signature</p>
                          </div>
                     </div>
-                    <div className='grid grid-cols-5 w-full text-center'>
-                        <div className='border-r-2 border-t-2 border-black h-8'></div>
-                        <div className='border-r-2 border-t-2 border-black h-8'></div>
-                        <div className='border-r-2 border-t-2 border-black h-8'></div>
-                        <div className='border-r-2 border-t-2 border-black h-8'></div>
-                        <div className='border-t-2 border-black h-8'></div>
-                    </div>
                 </CardFooter>
             </Card>
         </div>
     );
 });
 InvoicePreview.displayName = "InvoicePreview";
-
-    

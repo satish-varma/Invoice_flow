d5daff8d7a1394b9233ad1a270b5cabb3297c630
thegut guru <thegutguru.in@gmail.com>
2025-08-16 18:41:07 +0000

    remove the gap shown in the image

--- src/components/invoice-preview.tsx
+++ src/components/invoice-preview.tsx
@@ -60,7 +60,7 @@ export const InvoicePreview = React.forwardRef<HTMLDivElement, InvoicePreviewPro
                     </div>
                 </CardHeader>
                 <CardContent className="p-6">
-                    <Separator className='my-4 bg-gray-200'/>
+                    <Separator className='bg-gray-200'/>
                     <div className="grid grid-cols-[65%_35%] gap-8 items-center">
                         <div className="space-y-1">
                             <p className='font-bold text-gray-500'>Bill To:</p>
